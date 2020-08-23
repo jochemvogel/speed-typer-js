9cee15946d113c3f9c3045964eafc53a328b5b4e
Jochem Vogel <voge0031@hz.nl>
2020-08-23 11:38:41 +0200

    Remove temporary chrome aler

--- script.js
+++ script.js
@@ -11,12 +11,6 @@ const endBtn = document.getElementById('btn-end-game');
 const endScore = document.getElementById('end-score');
 const difficultyArea = document.getElementById('difficulty-area-span');
 
-// Temporary alert for Chrome Users
-const isChrome = !!window.chrome;
-if (isChrome) {
-  alert("I don't know what's going on, but I'm aware of the styling problems in Chrome 83. They fixed it in Canary (new version), so give it some time and it'll work again. My apologies.");
-}
-
 // Init
 const words = [];
 let randomWord;
